e0c7e12799fc880f0b185a43b8c209bcc8b90023
Ignacio Burgueño <ignaciob@inconcertcc.com>
2011-03-09 17:02:10 -0200

    Fixed test runner

--- unittest/luanode/run.sh
+++ unittest/luanode/run.sh
@@ -2,7 +2,7 @@
 
 for i in $( ls -1 --hide=disabled tests ); do
 	echo "\033[32mRunning test case: tests."$i"\033[0m" 
-	luanode run.lua simple.$i
+	luanode run.lua tests.$i
 	if [ $? -ne 0 ]
 	then
 		echo -e "\033[1m\033[41mTest case failed: " $i "\033[0m"
