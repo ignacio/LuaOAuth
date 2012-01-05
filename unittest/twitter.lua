d454d3a2e76fec83fdb9edfdcde6525ff2a75ad5
Christian G. Warden <cwarden@xerus.org>
2012-01-05 15:41:23 -0800

    Display authorization URLs for Twitter and Google
    Display authorization URLs for Twitter and Google for manual testing of
    the authorization process.  The query string should only contain the
    oauth_token parameter.

--- unittest/twitter.lua
+++ unittest/twitter.lua
@@ -6,10 +6,14 @@ local consumer_secret = "your consumer secret (you need to change this)"
 function test()
 	local client = OAuth.new(consumer_key, consumer_secret, {
 		RequestToken = "http://api.twitter.com/oauth/request_token", 
+		AuthorizeUser = {"https://api.twitter.com/oauth/authorize", method = "GET"},
 		AccessToken = "http://api.twitter.com/oauth/access_token"
 	})
 	print("requesting token")
 	local values = client:RequestToken()
+	local auth_url = client:BuildAuthorizationUrl()
+	print("Test authorization at the following URL")
+	print(auth_url)
 	assert_table(values)
 	assert_string(values.oauth_token)
 	assert_string(values.oauth_token_secret)
