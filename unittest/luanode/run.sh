66971ab492c42dae577707225e3e9621ce64a27b
Ignacio Burgueño <ignaciob@inconcertcc.com>
2014-06-02 15:41:35 -0300

    Fix typo

--- unittest/luanode/run.sh
+++ unittest/luanode/run.sh
@@ -7,12 +7,11 @@ declare -i has_error=0
 
 for i in $( ls -1 tests/*.lua ); do
 	echo -e "\033[32mRunning test case: tests."$i"\033[0m"
-	luanode_d run.lua $i
+	luanode run.lua $i
 	if [ $? -ne 0 ]
 	then
 		echo -e "\033[1m\033[41mTest case failed: " $i "\033[0m"
 		has_error=1
-		#break;
 	fi
 done
 
