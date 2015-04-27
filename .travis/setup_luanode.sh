c888cb7ea2b7e7a09b5f43bff1631828a55628f5
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-04-27 12:59:49 -0300

    Always use docker infrastructure on Travis
    We don't need to use a virtualized environment to run tests with LuaNode, since
    we can use the apt plugin in the test matrix (but in a somewhat convoluted way).

--- .travis/setup_luanode.sh
+++ .travis/setup_luanode.sh
@@ -2,8 +2,6 @@
 #
 # install luanode dependencies
 #
-sudo apt-get install libboost-dev libboost-system-dev libboost-date-time-dev libboost-thread-dev
-
 git clone --depth=1 --branch=master git://github.com/ignacio/LuaNode.git ~/luanode
 cd ~/luanode/build
 cmake -DBOOST_ROOT=/usr/lib ..
