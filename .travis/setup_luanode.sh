f8531f394fb4165efc1463e4a8f5bc6dadb89d45
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-04-27 12:15:23 -0300

    Update Travis integration code.
    Uses docker containers whenever possible (now the test run finishes faster).
    Sadly, we still don't have Lua 5.3 support due to LuaCrypto not supporting it.

--- .travis/setup_luanode.sh
+++ .travis/setup_luanode.sh
@@ -2,12 +2,11 @@
 #
 # install luanode dependencies
 #
-sudo luarocks install lunit
-sudo apt-get install libboost-dev libboost-system-dev libboost-date-time-dev libboost-thread-dev liblua5.1-json
+sudo apt-get install libboost-dev libboost-system-dev libboost-date-time-dev libboost-thread-dev
 
 git clone --depth=1 --branch=master git://github.com/ignacio/LuaNode.git ~/luanode
 cd ~/luanode/build
 cmake -DBOOST_ROOT=/usr/lib ..
 cmake --build .
-sudo mv luanode /usr/bin/luanode
-cd $TRAVIS_BUILD_DIR
+ln -s ~/luanode/build/luanode $HOME/.lua/luanode
+luanode -v
