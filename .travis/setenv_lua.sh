a7cb6790727eee8e72887cf20fe6e6ebfc91afe7
Ignacio Burgueño <ignaciob@inconcertcc.com>
2016-02-04 22:40:09 -0300

    Remove unused files [ci skip]

--- .travis/setenv_lua.sh
+++
@@ -1,7 +0,0 @@
-export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
-bash .travis/setup_lua.sh
-eval `$HOME/.lua/luarocks path`
-export LUA_DIR="$TRAVIS_BUILD_DIR/install/lua"
-if [ "$LUANODE" == "true" ]; then
-	bash .travis/setup_luanode.sh
-fi;
